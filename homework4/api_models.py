18268cc04ffe77166084decaa352b7761f7e3c87
vasilisal <61044287+vasilisal@users.noreply.github.com>
2020-06-16 11:36:41 +0300

    Add files via upload

---
+++ homework4/api_models.py
@@ -0,0 +1,25 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+class BaseUser(BaseModel):
+    """ Модель пользователя с базовыми полями """
+    id: int
+    first_name: str
+    last_name: str
+    online: int
+    deactivated: Optional[str]
+
+
+class User(BaseUser):
+    """ Модель пользователя с необязательным полем дата рождения """
+    bdate: Optional[str]
+
+
+class Message(BaseModel):
+    """ Модель сообщения """
+    user_id: int
+    peer_id: int
+    chat_id: int
+    message: str
+
